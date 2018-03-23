76e9254098e2f6d9fc3aad6dd1852a1fa8d40d2c
Simon Heiss <simonheiss87@gmail.com>
2018-03-23 17:32:52 +0100

    added create test

--- Application.Tests/CommandHandlerTests.cs
+++ Application.Tests/CommandHandlerTests.cs
@@ -92,5 +92,45 @@ namespace Application.Tests
             var result = await userCommandHandler.GetUsers();
             Assert.AreEqual(userList, (List<User>)((OkObjectResult)result).Value);
         }
+
+        [TestMethod]
+        public async Task CreateUser_HappyPath()
+        {
+            var eventStore = new Mock<IEventStore>();
+            eventStore.Setup(store => store.AppendAll(It.IsAny<List<DomainEventBase>>()))
+                .ReturnsAsync(HookResult.OkResult());
+            var postRepo = new Mock<IPostRepository>();
+
+            var userRepo = new Mock<IUserRepository>();
+
+            var userCreateCommand = new UserCreateCommand("Peter", 18);
+            var createdEntity = User.Create(userCreateCommand).CreatedEntity;
+            userRepo.Setup(repo => repo.CreateUser(createdEntity)).Returns(Task.FromResult(true));
+
+            var userCommandHandler = new UserCommandHandler(eventStore.Object, userRepo.Object, postRepo.Object);
+
+            var result = await userCommandHandler.CreateUser(userCreateCommand);
+            Assert.AreEqual(201, ((CreatedResult)result).StatusCode);
+            Assert.AreEqual(18, ((User)((CreatedResult)result).Value).Age);
+            Assert.AreEqual("Peter", ((User)((CreatedResult)result).Value).Name);
+        }
+
+        [TestMethod]
+        public async Task CreateUser_HookFails()
+        {
+            var eventStore = new Mock<IEventStore>();
+            var errors = new List<string> { "Some Error" };
+            eventStore.Setup(store => store.AppendAll(It.IsAny<List<DomainEventBase>>()))
+                .ReturnsAsync(HookResult.ErrorResult(errors));
+            var postRepo = new Mock<IPostRepository>();
+
+            var userRepo = new Mock<IUserRepository>();
+
+            var userCommandHandler = new UserCommandHandler(eventStore.Object, userRepo.Object, postRepo.Object);
+
+            var result = await userCommandHandler.CreateUser(new UserCreateCommand("Peter", 18));
+            Assert.AreEqual(400, ((BadRequestObjectResult)result).StatusCode);
+            Assert.AreEqual(errors, (List<string>)((BadRequestObjectResult)result).Value);
+        }
     }
 }
