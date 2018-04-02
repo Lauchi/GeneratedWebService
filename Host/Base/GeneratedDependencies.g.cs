a4bf8774a5871b89740f59205d2f60fa92b5fd2e
Simon Heiss <simonheiss87@gmail.com>
2018-04-02 10:59:48 +0200

    added user query back to di

--- Host/Base/GeneratedDependencies.g.cs
+++ Host/Base/GeneratedDependencies.g.cs
@@ -31,6 +31,7 @@ namespace Host
             collection.AddTransient<IEventStore, EventStore>();
             collection.AddTransient<IEventStoreRepository, EventStoreRepository>();
             collection.AddTransient<EventJobRegistration>();
+            collection.AddTransient<UserQuery>();
             collection.AddTransient<IHangfireQueue, HangfireQueue>();
             collection.AddMvc().AddApplicationPart(typeof(UserController).Assembly);
             collection.AddTransient<IUserRepository, UserRepository>();
